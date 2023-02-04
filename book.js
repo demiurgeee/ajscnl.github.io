bf5dcc593dc63b1bc3a73f31f930f2d104a54144
demiurgeee <124445496+demiurgeee@users.noreply.github.com>
2023-02-04 23:02:56 +0800

    Add files via upload

---
+++ book.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById('id02');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function booking(){
+    document.location.href="BOOKING.HTML"
+}
